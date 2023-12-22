a15af3f826bd30126e973cc40413869c687f4593
Parthiban K <41236775+parthibankrpa@users.noreply.github.com>
2023-12-22 16:19:48 +0530

    with simple msg

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <App />
+    <h1>hi</h1>
   </React.StrictMode>,
 )

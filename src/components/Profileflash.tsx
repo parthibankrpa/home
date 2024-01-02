99dfd0940d08ada1feeb10efa54ff5163f4a9419
Parthiban K <41236775+parthibankrpa@users.noreply.github.com>
2024-01-02 15:03:09 +0530

    with styles

--- src/components/Profileflash.tsx
+++ src/components/Profileflash.tsx
@@ -1,24 +1,34 @@
 import data from "../data/resume.json";
 import { Row, Col, Container, Button } from 'react-bootstrap';
 import ProfileImage from "./ProfileImage";
+import SocialConnect from "./SocialConnect";
 
 
 function Profileflash() {
     return <Container className="flash-container">
         <Row>
-            <Col sm={6}>
-                <h5>Hello, I'm</h5>
-                <h2 className="mb-0 d-block d-lg-block">{data.name}</h2>
-                <p className="summary">{data.professional_summary}</p>
-                <div>
-                    <a href="./resume.pdf" download><Button variant="primary">Download Cv</Button></a>
-                </div>
-
+            <Col sm={8}>
+                <Row>
+                    <h5 className="profile-title">Hello, I'm</h5>
+                </Row>
+                <Row>
+                        <h2 className="profile-name">{data.name}</h2>
+                </Row>
+                <Row>
+                    <p className="summary">{data.professional_summary}</p>
+                </Row>
+                <Row>
+                    <Col>
+                        <a href="./resume.pdf" download><Button variant="outline-light" className="btn-outline" >Download Cv</Button></a>
+                    </Col>
+                    <Col>
+                    <SocialConnect/>
+                    </Col>
+                </Row>
             </Col>
             <Col className="profile-image-container">
                 <ProfileImage />
             </Col>
-
         </Row >
     </Container >;
 }
--- src/components/SocialConnect.tsx
+++ src/components/SocialConnect.tsx
@@ -1,13 +1,11 @@
-function SocialConnect(){
-    return <div className="atf-banner-social">
-    <ul className="atf-banner-icon  list-unstyled">
-        <li className="facebook aos-init" data-aos="fade-in" data-aos-duration="1000"><a title="Facebook" href="#"><i className="fab fa-facebook"></i></a></li>
-        <li className="linkedin aos-init" data-aos="fade-in" data-aos-duration="1000"><a title="Linkedin" href="#"><i className="fab fa-linkedin"></i></a></li>
-        <li className="twitter aos-init" data-aos="fade-in" data-aos-duration="1000"><a title="Twitter" href="#"><i className="fab fa-twitter"></i></a></li>
-        <li className="instagram aos-init" data-aos="fade-in" data-aos-duration="1000"><a title="Instagram" href="#"><i className="fab fa-instagram"></i></a></li>
-        <li className="whatsapp aos-init" data-aos="fade-in" data-aos-duration="1000"><a title="Whatsapp" href="#"><i className="fab fa-whatsapp"></i></a></li>
-    </ul>
-</div>;
+function SocialConnect() {
+    return <div className="social-connect">
+        <ul className="atf-banner-icon  list-unstyled">
+            <li><a title="Linkedin" href="https://www.linkedin.com/in/parthibankrpa"><i className="fab fa-linkedin"></i></a></li>
+            <li><a title="Whatsapp" href="#"><i className="fab fa-whatsapp"></i></a></li>
+            <li><a title="GitHub" href="https://github.com/parthibankrpa"><i className="fab fa-github"></i></a></li>
+        </ul>
+    </div>;
 }
 
 export default SocialConnect;

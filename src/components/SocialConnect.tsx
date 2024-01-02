afa95fc5321e1404aaae0601b93a5d4b44cba681
Parthiban K <41236775+parthibankrpa@users.noreply.github.com>
2024-01-02 15:08:27 +0530

    with social connect styles

--- src/components/SocialConnect.tsx
+++ src/components/SocialConnect.tsx
@@ -1,11 +1,33 @@
 function SocialConnect() {
-    return <div className="social-connect">
-        <ul className="atf-banner-icon  list-unstyled">
-            <li><a title="Linkedin" href="https://www.linkedin.com/in/parthibankrpa"><i className="fab fa-linkedin"></i></a></li>
-            <li><a title="Whatsapp" href="#"><i className="fab fa-whatsapp"></i></a></li>
-            <li><a title="GitHub" href="https://github.com/parthibankrpa"><i className="fab fa-github"></i></a></li>
-        </ul>
-    </div>;
+  return (
+    <div className="social-connect">
+      <ul className="atf-banner-icon  list-unstyled">
+        <li>
+          <a
+            title="Linkedin"
+            href="https://www.linkedin.com/in/parthibankrpa"
+            target="_blank"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a title="Whatsapp" href="https://wa.me/918904757529" target="_blank">
+            <i className="fab fa-whatsapp"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            title="GitHub"
+            href="https://github.com/parthibankrpa"
+            target="_blank"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SocialConnect;
